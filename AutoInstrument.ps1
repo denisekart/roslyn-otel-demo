f13ff4bd37948432d8457d28872f759bb10a5b02
Denis Ekart <denis.ekart@gmail.com>
2023-09-25 02:39:01 +0200

    feat: add auto instrumentation script chore: clean up code

--- AutoInstrument.ps1
+++ AutoInstrument.ps1
@@ -10,10 +10,18 @@ Import-Module $download_path
 Install-OpenTelemetryCore
 
 # Set up the instrumentation for the current PowerShell session
-Register-OpenTelemetryForCurrentSession -OTelServiceName "GitHubStatsWebApi"
+Register-OpenTelemetryForCurrentSession -OTelServiceName "MyServiceDisplayName"
 
-$Env:ASPNETCORE_URLS = "http://localhost:5003"
-$Env:ASPNETCORE_CONTENTROOT = ".\src\GitHubStatsWebApi\bin\Debug\net7.0"
+$Env:ASPNETCORE_URLS = "http://localhost:5171"
+$Env:ASPNETCORE_CONTENTROOT = "."
+$Env:ASPNETCORE_ENVIRONMENT = "Development"
+$Env:OTEL_DOTNET_AUTO_FAIL_FAST_ENABLED = 1
+$Env:OTEL_DOTNET_AUTO_RESOURCE_DETECTOR_ENABLED = 1
+$Env:OTEL_TRACES_SAMPLER = "always_on"
+$Env:OTEL_TRACES_EXPORTER = "otlp"
+$Env:OTEL_EXPORTER_OTLP_PROTOCOL = "grpc"
+$Env:OTEL_DOTNET_AUTO_TRACES_ENABLED = 1
+
 
 # Run your application with instrumentation
 .\src\GitHubStatsWebApi\bin\Debug\net7.0\GitHubStatsWebApi.exe
